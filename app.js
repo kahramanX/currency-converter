36e5842d94430acfe96123e0361fa260de85bc6f
Mustafa Kahraman <80291704+kahramanX@users.noreply.github.com>
2021-12-04 21:46:22 +0300

    finding chosen currency

--- app.js
+++ app.js
@@ -14,25 +14,25 @@ app.set('view engine', 'ejs');
 //public access
 app.use("/public", express.static('public'));
 
+//json
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
 // route
 app.get("/", (req, res) => {
     res.cookie("count", count++);
 
-
-    console.log(req.body);
-
-    let currency = "";
-
-    axios.get(`https://freecurrencyapi.net/api/v2/latest?apikey=${myAPIkey}`, {
-            params: {
-                base_currency: `${currency}`
-            }
-        })
+    axios.get(`https://freecurrencyapi.net/api/v2/latest?apikey=${myAPIkey}`)
         .then((response) => {
 
             let currencies = Object.keys(response.data.data);
 
-            res.render("index",{currencies});
+            res.render("index", {
+                currencies
+            });
         })
         .catch((err) => {
             console.log(err)
@@ -41,6 +41,38 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
     console.log(req.body);
+    console.log("üstteki post işleminden");
+
+    let {
+        currValue,
+        from,
+        to
+    } = req.body;
+
+    axios.get(`https://freecurrencyapi.net/api/v2/latest?apikey=${myAPIkey}`, {
+            params: {
+                base_currency: `${from}`
+            }
+        })
+        .then((response) => {
+
+            let currencies = response.data.data;
+
+            let asArray = Object.entries(currencies);
+
+            asArray.filter(([key, value]) => {
+                if (key == to) {
+                    console.log(`adı=${key} - value=${value}`)
+                }
+            })
+
+            //let newScore = Object.fromEntries(isTOEqualToAPI);
+            //console.log(newScore)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
 })
 // server
 app.listen(process.env.PORT || 5000, () => {
