0991b56e3e057c9f76c8bf4a57bedd43d690ca90
Mustafa Kahraman <80291704+kahramanX@users.noreply.github.com>
2021-12-06 01:29:13 +0300

    i blocked submit empty value for button

--- public/js/script.js
+++ public/js/script.js
@@ -2,23 +2,37 @@ class SelectCurrency {
     constructor() {
         this.leftSelect = document.querySelectorAll('select')[0];
         this.rightSelect = document.querySelectorAll('select')[1];
+
+        this.disabledButton = document.querySelector("button");
     }
 
     allEventListeners() {
         this.leftSelect.addEventListener('change', this.leftSelectChange);
-        this.rightSelect.addEventListener('change', this.rightSelectChange);
+        this.rightSelect.addEventListener('change', this.rightSelectChange.bind(this));
     }
 
     leftSelectChange(e) {
         let selectedValue = e.target.value;
 
         console.log(e.target.value);
+
+        let isDisabled = true;
     }
 
     rightSelectChange(e) {
         let selectedValue = e.target.value;
 
         console.log(e.target.value);
+
+        this.checkDisabled(true, true);
+    }
+
+
+    checkDisabled(par1, par2) {
+        if (par1 && par2 == true) {
+
+            this.disabledButton.disabled = false;
+        }
     }
 }
 
